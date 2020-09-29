86caa1b060c12198b1c6fd66396a70fc2bbf7352
Ekaansh Arora <blue.name68@gmail.com>
2020-09-29 21:39:09 +0530

    Tooltips added

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import {Button, Grid, Tooltip} from '@material-ui/core';
 import type {LogoStateType, LogoType} from '../pages/console';
+import InfoIcon from '@material-ui/icons/Info';
 
 interface UploadProps {
   label: string;
   file: LogoStateType;
   name: LogoType;
   handler: (file: LogoStateType, name: LogoType) => void;
+  tip: string;
 }
 
 export default function Upload(props: UploadProps) {
@@ -37,21 +39,38 @@ export default function Upload(props: UploadProps) {
 
   return (
     <>
-      <Button variant="outlined" color="primary" component="label">
-        {props.label}
-        <input
-          type="file"
-          onChange={handleFileUpload}
-          onClick={onInputClick}
-          style={{display: 'none'}}
-          accept={extensions.reduce(
-            (acc, curr, idx) => `${idx === 1 ? '.' : ''}${acc},.${curr}`,
+      <Grid container spacing={0} alignItems="center">
+        <Grid xs={11} item>
+          <Button
+            variant="outlined"
+            color="primary"
+            component="label"
+            style={{width: '75%'}}>
+            {props.label}
+            <input
+              type="file"
+              onChange={handleFileUpload}
+              onClick={onInputClick}
+              style={{display: 'none'}}
+              accept={extensions.reduce(
+                (acc, curr, idx) => `${idx === 1 ? '.' : ''}${acc},.${curr}`,
+              )}
+            />
+          </Button>
+        </Grid>
+        <Grid xs={1} alignItems="flex-start" item>
+          {props.tip ? (
+            <Tooltip title={props.tip} arrow>
+              <InfoIcon color={'secondary'} />
+            </Tooltip>
+          ) : (
+            <></>
           )}
-        />
-      </Button>
-      <p color="primary">
-        {props.file ? props.file.name : 'No file is selected'}
-      </p>
+        </Grid>
+        <p color="primary" style={{textAlign: 'center', margin: '8px auto 12px auto'}}>
+          {props.file ? props.file.name : 'No file is selected'}
+        </p>
+      </Grid>
     </>
   );
 }
