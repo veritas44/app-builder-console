d377361ae1f5a1b6239475eb4c3d881e3c84a2ee
adictya <aditya.26101@gmail.com>
2021-02-10 17:40:50 +0530

    drawer fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import React, {useState, useRef, useEffect} from 'react';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-import {Button, IconButton, Paper, SvgIcon} from '@material-ui/core';
+import {Button, IconButton, Paper, SwipeableDrawer, Typography} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import {useRouter} from 'next/router';
 import useDrawerToggle from '../hooks/drawerToggle';
+import SideBarContent from '../MDXComponents/SidebarContent';
 // import {getLeftDrawerToggle , setLeftDrawerToggle} from '../hooks/drawerToggle';
 import Link from './Link';
 import useSmQuerry from '../hooks/useSmQuerry';
@@ -58,6 +59,17 @@ const useStyles = makeStyles((theme: Theme) =>
     rightButton: {
       marginLeft: '10px',
     },
+    customDrawer: {
+      width: '70vw',
+    },
+    textStyle: {
+      color: 'white',
+      marginTop:"10px",
+      marginLeft:"10px",
+    },
+    toolbar: {
+      height: '3rem',
+    },
   }),
 );
 
@@ -93,9 +105,10 @@ function NavButton(props: navButtonProps) {
 }
 
 export default function Header() {
+  const router = useRouter();
   const matches = useSmQuerry();
   const [isSticky, setSticky] = useState(false);
-  const [leftDrawerToggle, setLeftDrawerToggle] = useDrawerToggle(false);
+  const [leftDrawerToggle, setLeftDrawerToggle] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const handleScroll = () => {
     if (ref.current) {
@@ -120,10 +133,30 @@ export default function Header() {
       <Paper className={classes.appBar} elevation={isSticky ? 3 : 0} square>
         <Container className={classes.root}>
           <div className={classes.leftSection}>
-            {matches ? (
-              <IconButton onClick={() => leftDrawerVisible(!leftDrawerVisible)}>
-                <MenuIcon color="primary" />
-              </IconButton>
+            {matches && router.pathname.split('/')[1] == 'docs' ? (
+              <>
+                <IconButton>
+                  <MenuIcon color="primary" />
+                </IconButton>
+                <SwipeableDrawer
+                  anchor="left"
+                  classes={{paperAnchorLeft: classes.customDrawer}}
+                  open={leftDrawerToggle}
+                  onClose={() => setLeftDrawerToggle(false)}
+                  onOpen={() => setLeftDrawerToggle(true)}
+                  >
+                  <div
+                    style={{backgroundColor: '#079dfd'}}
+                    className={classes.toolbar}>
+                    <Typography
+                      className={classes.textStyle}
+                      variant={'h2'}>
+                      Navigation
+                    </Typography>
+                  </div>
+                  <SideBarContent />
+                </SwipeableDrawer>
+              </>
             ) : (
               ''
             )}
@@ -165,16 +198,45 @@ export default function Header() {
               color="primary"
               disableElevation>
               {matches ? (
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.05962 15.1722C5.05962 16.5671 3.93312 17.6945 2.53933 17.6945C1.14553 17.6945 0.019043 16.5671 0.019043 15.1722C0.019043 13.7773 1.14553 12.6499 2.53933 12.6499H5.05962V15.1722Z" fill="#E01E5A"/>
-<path d="M6.31982 15.1722C6.31982 13.7773 7.44632 12.6499 8.84011 12.6499C10.2339 12.6499 11.3604 13.7773 11.3604 15.1722V21.4779C11.3604 22.8728 10.2339 24.0002 8.84011 24.0002C7.44632 24.0002 6.31982 22.8728 6.31982 21.4779V15.1722Z" fill="#E01E5A"/>
-<path d="M8.84011 5.04458C7.44632 5.04458 6.31982 3.91719 6.31982 2.52229C6.31982 1.12739 7.44632 0 8.84011 0C10.2339 0 11.3604 1.12739 11.3604 2.52229V5.04458H8.84011Z" fill="#36C5F0"/>
-<path d="M8.8401 6.32471C10.2339 6.32471 11.3604 7.45209 11.3604 8.847C11.3604 10.2419 10.2339 11.3693 8.8401 11.3693H2.52029C1.12649 11.3693 0 10.2419 0 8.847C0 7.45209 1.12649 6.32471 2.52029 6.32471H8.8401Z" fill="#36C5F0"/>
-<path d="M18.9404 8.847C18.9404 7.45209 20.0669 6.32471 21.4607 6.32471C22.8545 6.32471 23.981 7.45209 23.981 8.847C23.981 10.2419 22.8545 11.3693 21.4607 11.3693H18.9404V8.847Z" fill="#2EB67D"/>
-<path d="M17.6802 8.84712C17.6802 10.242 16.5537 11.3694 15.1599 11.3694C13.7661 11.3694 12.6396 10.242 12.6396 8.84712V2.52229C12.6396 1.12739 13.7661 0 15.1599 0C16.5537 0 17.6802 1.12739 17.6802 2.52229V8.84712Z" fill="#2EB67D"/>
-<path d="M15.1599 18.9556C16.5537 18.9556 17.6802 20.083 17.6802 21.4779C17.6802 22.8728 16.5537 24.0001 15.1599 24.0001C13.7661 24.0001 12.6396 22.8728 12.6396 21.4779V18.9556H15.1599Z" fill="#ECB22E"/>
-<path d="M15.1599 17.6945C13.7661 17.6945 12.6396 16.5671 12.6396 15.1722C12.6396 13.7773 13.7661 12.6499 15.1599 12.6499H21.4797C22.8735 12.6499 24 13.7773 24 15.1722C24 16.5671 22.8735 17.6945 21.4797 17.6945H15.1599Z" fill="#ECB22E"/>
-</svg>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M5.05962 15.1722C5.05962 16.5671 3.93312 17.6945 2.53933 17.6945C1.14553 17.6945 0.019043 16.5671 0.019043 15.1722C0.019043 13.7773 1.14553 12.6499 2.53933 12.6499H5.05962V15.1722Z"
+                    fill="#E01E5A"
+                  />
+                  <path
+                    d="M6.31982 15.1722C6.31982 13.7773 7.44632 12.6499 8.84011 12.6499C10.2339 12.6499 11.3604 13.7773 11.3604 15.1722V21.4779C11.3604 22.8728 10.2339 24.0002 8.84011 24.0002C7.44632 24.0002 6.31982 22.8728 6.31982 21.4779V15.1722Z"
+                    fill="#E01E5A"
+                  />
+                  <path
+                    d="M8.84011 5.04458C7.44632 5.04458 6.31982 3.91719 6.31982 2.52229C6.31982 1.12739 7.44632 0 8.84011 0C10.2339 0 11.3604 1.12739 11.3604 2.52229V5.04458H8.84011Z"
+                    fill="#36C5F0"
+                  />
+                  <path
+                    d="M8.8401 6.32471C10.2339 6.32471 11.3604 7.45209 11.3604 8.847C11.3604 10.2419 10.2339 11.3693 8.8401 11.3693H2.52029C1.12649 11.3693 0 10.2419 0 8.847C0 7.45209 1.12649 6.32471 2.52029 6.32471H8.8401Z"
+                    fill="#36C5F0"
+                  />
+                  <path
+                    d="M18.9404 8.847C18.9404 7.45209 20.0669 6.32471 21.4607 6.32471C22.8545 6.32471 23.981 7.45209 23.981 8.847C23.981 10.2419 22.8545 11.3693 21.4607 11.3693H18.9404V8.847Z"
+                    fill="#2EB67D"
+                  />
+                  <path
+                    d="M17.6802 8.84712C17.6802 10.242 16.5537 11.3694 15.1599 11.3694C13.7661 11.3694 12.6396 10.242 12.6396 8.84712V2.52229C12.6396 1.12739 13.7661 0 15.1599 0C16.5537 0 17.6802 1.12739 17.6802 2.52229V8.84712Z"
+                    fill="#2EB67D"
+                  />
+                  <path
+                    d="M15.1599 18.9556C16.5537 18.9556 17.6802 20.083 17.6802 21.4779C17.6802 22.8728 16.5537 24.0001 15.1599 24.0001C13.7661 24.0001 12.6396 22.8728 12.6396 21.4779V18.9556H15.1599Z"
+                    fill="#ECB22E"
+                  />
+                  <path
+                    d="M15.1599 17.6945C13.7661 17.6945 12.6396 16.5671 12.6396 15.1722C12.6396 13.7773 13.7661 12.6499 15.1599 12.6499H21.4797C22.8735 12.6499 24 13.7773 24 15.1722C24 16.5671 22.8735 17.6945 21.4797 17.6945H15.1599Z"
+                    fill="#ECB22E"
+                  />
+                </svg>
               ) : (
                 'Get support'
               )}
