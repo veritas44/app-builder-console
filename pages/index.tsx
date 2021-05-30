0f14615fbe7c601e5f8b31d8ea9b9b83b6eadfc9
technophilic <svineth.face@gmail.com>
2021-05-30 17:38:01 +0530

    update queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -407,11 +407,6 @@ function Home() {
               onClick={() => window.open('https://www.google.com/')}>
               <Box color={navWhite?"black":"white"}>Get Support</Box>
             </Button>
-            <Button
-              className={NavbarClasses.button}
-              onClick={() => window.open('http://sso2.staging.agora.io/api/v0/oauth/authorize?scope=basic_info&response_type=code&redirect_uri=https://rocky-temple-79220.herokuapp.com/auth&client_id=7a8f4c3d28fa40f6b506a2725c2a81e8')}>
-              Login
-            </Button>
             <Link href="/create">
               <Button
                 className={NavbarClasses.button}
