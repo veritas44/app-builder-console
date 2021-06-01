b3bea8cc6726a30ba8fc5b1ca5a0eecf355e1111
technophilic <svineth.face@gmail.com>
2021-06-01 22:00:44 +0530

    new version

--- config/PerformAPI.tsx
+++ config/PerformAPI.tsx
@@ -554,7 +554,7 @@ const convertToVercel = (code: String, varcelState: any) => {
       keywords: [],
       license: 'MIT',
       dependencies: {
-        'agora-app-builder-cli': '0.0.16',
+        'agora-app-builder-cli': '1.0.1',
       },
     },
     themeJson: themeJson,
